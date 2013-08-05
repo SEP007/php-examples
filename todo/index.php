fa46232333d2722cbf091c73acdaf88c86c70426
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-05 17:33:42 +0200

    Adds html-app-titles.

---
+++ todo/index.php
@@ -0,0 +1,18 @@
+<html class='no-js'>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Die Todo App</title>
+        <meta name='description' content=''>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>
+    <body>
+        <?php
+            error_reporting(E_ALL ^ E_NOTICE);
+            setlocale(LC_ALL, "de_DE");
+            date_default_timezone_set('Europe/Berlin');
+
+
+        ?>
+    </body>
+</html>
